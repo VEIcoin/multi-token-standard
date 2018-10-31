8de20f32742e5b947e83330dc1ee473c4d60b0ee
José Carlos Nieto <jose.carlos@menteslibres.net>
2018-10-31 14:45:52 +0000

    fix build errors and rename to multi-token-standard

--- contracts/mocks/MFTMockX.sol
+++ contracts/mocks/MFTMockX.sol
@@ -3,7 +3,7 @@ pragma experimental ABIEncoderV2;
 
 import '../token/MFTX.sol';
 
-contract MFTXMock is MFTX {
+contract MFTMockX is MFTX {
 
   function mockMint(address _address, uint256 _type, uint256 _value) public {
     _updateTypeBalance(_address, _type, _value, Operations.Add);
